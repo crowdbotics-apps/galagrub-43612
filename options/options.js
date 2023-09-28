2d4201ffddfb64638bdc20e7aa2c7417df5e63b0
Crowdbotics <team@crowdbotics.com>
2023-09-28 17:46:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "galagrub-43612",
+  url: "https://galagrub-43612.botics.co",
+  api: "https://galagrub-43612.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
